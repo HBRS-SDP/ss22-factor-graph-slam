a13662ac0be9c0b403a4c6acef93ef0960e4753e
RavisankarSelvaraju <ravisankar.selvaraju@smail.inf.h-brs.de>
2022-06-09 17:30:23 +0200

    update data_loger and sample collected file #15

--- gtsam/data_collection/scripts/data_logger.py
+++ gtsam/data_collection/scripts/data_logger.py
@@ -1,3 +1,4 @@
+from cmath import nan
 from pickle import FALSE
 import time
 
@@ -12,114 +13,102 @@ from csv import writer
 
 
 
-path = "/home/ravi/Desktop/Work/semester_2/sdp/Sdp_factor_graphs/gtsam/data_collection/Extracted_data_from_bag_files/sdp_data_collection_"+str(time.time())+".csv"
+path = "/home/ravi/Desktop/Work/semester_2/sdp/Sdp_factor_graphs/gtsam/data_collection/Extracted_data_from_bag_files/sdp_data_collection_"+"look_one_3_marker"+".csv"
 count = 0
 cmd_cnt = 0
-flag_fidu = True
-flag_cmd = True
+flag = True
 rostime = None
-# common_access_flag = [True,True]
-data_to_write = []
+
+
+data =  [count,'-',rostime,'-','-','-','-','-','-','-','-','-','-','-','-','-','-']
 
 
 def data_writer(data_to_write):
 
+    global data
+    global count
+
+    if not flag:
+        count+=1
+        data_to_write[0] = count
     with open(path, 'a+') as f_object:
 
         writer_object = writer(f_object)
         writer_object.writerow(data_to_write)
         f_object.close()
 
-    data_to_write = []
 
 
 def callback_fiducial_transform(msg):
 
     global count
-    global value
-    global flag_fidu
-    global data_to_write
-    global fiducial
+    global flag
     global rostime
 
-    count += 1
-
-    data_dict = {'S.no' : [],
-                'aruco_id': [],
-                'Timestamp': [],
-                'score':[],
-                'p_x':[],
-                'p_y':[],
-                'p_z':[],
-                'o_x':[],
-                'o_y':[],
-                'o_z':[],
-                'o_w':[]}
+    global data 
 
     detections = msg.detections
     header = msg.header
     rostime = datetime.fromtimestamp(header.stamp.secs)
 
-    if flag_fidu:
-        data = list(data_dict.keys())
-        data_writer(data)
-        data = []
-        flag_fidu = False
+    if flag:
+        data_header = ['S.no', 'aruco_id', 'Timestamp', 'score', 'p_x', 'p_y', 'p_z', 'o_x', 'o_y', 'o_z', 'o_w','x_linear','y_linear','z_linear','x_angular','y_angular','z_angular']
+        data_writer(data_header)
+        flag = False
 
 
     if detections == []:
 
-        data = [count,0,rostime,0,0,0,0,0,0,0,0]
-        data_writer(data)
-
+        data[1] = "-"
+        data[2] = rostime
+        data[3] = "-"
+        data[4] = "-"
+        data[5] = "-"
+        data[6] = "-"
+        data[7] = "-"
+        data[8] = "-"
+        data[9] = "-"
+        data[10] = "-"
 
     elif detections[0].results[0].score*100 > 10:
 
-        data = []
-
-        data.append(count)
-        data.append(detections[0].results[0].id)
-        data.append(rostime)
-        data.append(detections[0].results[0].score)
-        data.append(detections[0].results[0].pose.pose.position.x)
-        data.append(detections[0].results[0].pose.pose.position.y)
-        data.append(detections[0].results[0].pose.pose.position.z)
-        data.append(detections[0].results[0].pose.pose.orientation.x)
-        data.append(detections[0].results[0].pose.pose.orientation.y)
-        data.append(detections[0].results[0].pose.pose.orientation.z)
-        data.append(detections[0].results[0].pose.pose.orientation.w)
+        data[1] = (detections[0].results[0].id)
+        data[2] = rostime
+        data[3] = detections[0].results[0].score
+        data[4] = detections[0].results[0].pose.pose.position.x
+        data[5] = detections[0].results[0].pose.pose.position.y
+        data[6] = detections[0].results[0].pose.pose.position.z
+        data[7] = detections[0].results[0].pose.pose.orientation.x
+        data[8] = detections[0].results[0].pose.pose.orientation.y
+        data[9] = detections[0].results[0].pose.pose.orientation.z
+        data[10] = detections[0].results[0].pose.pose.orientation.w
 
         data_writer(data)
-        data = []
 
 def callback_cmd_vel(msg):
 
-    global flag_cmd
-    global cmd_cnt
+    global flag
+    global count
+    global rostime
+    global data
 
-    data = []
-    if flag_cmd:
 
-        data = [count,0,rostime,0,0,0,0,0,0,0,0,'S.no','x_linear','y_linear','z_linear','x_angular','y_angular','z_angular']
-        flag_cmd = False
-        data_writer(data)
-        data = []
+    if flag:
 
-    data = [count,0,rostime,0,0,0,0,0,0,0,0]
-    cmd_cnt += 1
-    data.append(cmd_cnt)
-    data.append(msg.linear.x)
-    data.append(msg.linear.y)
-    data.append(msg.linear.z)
+        data_header = ['S.no', 'aruco_id', 'Timestamp', 'score', 'p_x', 'p_y', 'p_z', 'o_x', 'o_y', 'o_z', 'o_w','x_linear','y_linear','z_linear','x_angular','y_angular','z_angular']
+        data_writer(data_header)
 
-    data.append(msg.angular.x)
-    data.append(msg.angular.y)
-    data.append(msg.angular.z)
+        flag = False
+
+    data[-6] = msg.linear.x
+    data[-5] = msg.linear.y
+    data[-4] = msg.linear.z
+
+    data[-3] = msg.angular.x
+    data[-2] = msg.angular.y
+    data[-1] = msg.angular.z
 
     data_writer(data)
-    data = []
-
-
  
 def listener():
 
